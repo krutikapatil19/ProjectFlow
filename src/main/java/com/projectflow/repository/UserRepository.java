ba85da97ed2961c9557140df3c4acf4521524441
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-07-24 19:06:15 +0530

    User Repository.(to talk with the database-automatically done.)

--- src/main/java/com/projectflow/repository/UserRepository.java
+++ src/main/java/com/projectflow/repository/UserRepository.java
@@ -4,8 +4,10 @@ import com.projectflow.projectflow.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
+//By extending JpaRepository<User, Long> we instantly get methods like save(user) - insert or update a user , findById(id), findAll(), deleteById(id)
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    //findBy + Email -> Spring understands find a User where the email field matches whats passed in.
     Optional<User> findByEmail(String email);
 
 }
