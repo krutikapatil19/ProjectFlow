53cdbce66d493fa4118142630c4040905f6b093d
krutikapatil19 <krutikapatil.1911@gmail.com>
2026-07-24 19:22:13 +0530

    User Service.

---
+++ src/main/java/com/projectflow/projectflow/service/UserService.java
@@ -0,0 +1,36 @@
+package com.projectflow.projectflow.service;
+
+import com.projectflow.projectflow.entity.User;
+import com.projectflow.projectflow.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public Optional<User> getUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
